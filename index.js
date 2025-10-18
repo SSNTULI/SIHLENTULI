1c9b4d272ad29929ebbc5a759855fa26f228f372
SSNTULI <sihlentuli49@gmail.com>
2025-10-18 22:19:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+// $(".ntuli-kid").on("click",function(){
+//     alert("Page under maintenance,\n Please try again later");
+// });
---
+++ projects.js
@@ -0,0 +1,3 @@
+$(".project-more").on("click",function(){
+    alert("Details not available. \nTransnet Freight Rail intellectual Property");
+});
